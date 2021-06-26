d543ee9b8caba858a3d83d9d5fc283acb2b51070
Tinko <Lyderslev1@gmail.com>
2021-06-27 00:10:17 +0200

    Found out i was calling the wrong thing. It can now count the amount of items in the counter array.

--- LugageSorterGUI/MainWindow.xaml.cs
+++ LugageSorterGUI/MainWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace LugageSorterGUI
                                 if (((CheckinQueueEvent)e).CounterNumber == 0 && ((CheckinQueueEvent)e).Status)
                                 {
                                     //Looks at our label (counter1) and checks if the event, e, and checks our queue, and tries to get the amount and make it a string.
-                                    lbl_Counter1Queue.Content = ((CheckinQueueEvent)e).CounterNumber;
+                                    lbl_Counter1Queue.Content = ((CheckinQueueEvent)e).Amount;
                                     lbl_Counter1Queue.Background = new SolidColorBrush(Colors.Green);
                                 }
                                 else
@@ -56,7 +56,7 @@ namespace LugageSorterGUI
                                 if (((CheckinQueueEvent)e).CounterNumber == 1 && ((CheckinQueueEvent)e).Status)
                                 {
                                     //Looks at our label (counter1) and checks if the event, e, and checks our queue, and tries to get the amount and make it a string.
-                                    lbl_Counter2Queue.Content = ((CheckinQueueEvent)e).CounterNumber;
+                                    lbl_Counter2Queue.Content = ((CheckinQueueEvent)e).Amount;
                                     lbl_Counter2Queue.Background = new SolidColorBrush(Colors.Green);
                                 }
                                 else
@@ -68,7 +68,7 @@ namespace LugageSorterGUI
                                 if (((CheckinQueueEvent)e).CounterNumber == 2 && ((CheckinQueueEvent)e).Status)
                                 {
                                     //Looks at our label (counter1) and checks if the event, e, and checks our queue, and tries to get the amount and make it a string.
-                                    lbl_Counter3Queue.Content = ((CheckinQueueEvent)e).CounterNumber;
+                                    lbl_Counter3Queue.Content = ((CheckinQueueEvent)e).Amount;
                                     lbl_Counter3Queue.Background = new SolidColorBrush(Colors.Green);
                                 }
                                 else
