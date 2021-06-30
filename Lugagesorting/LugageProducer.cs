c3f6dec3d3ee4fbd5e353bd7808660d475b7327f
Mads Tinko Madsen <mtm@itklyngen.dk>
2021-06-30 11:07:56 +0200

    Addde comments to lugage producer

--- Lugagesorting/LugageProducer.cs
+++ Lugagesorting/LugageProducer.cs
@@ -14,17 +14,25 @@ namespace Lugagesorting
         {
             while (Thread.CurrentThread.IsAlive)
             {
+                //Run through for counter array length
                 for (int i = 0; i < Manager.counters.Length; i++)
                 {
+                    //Create a counter for each run through
                     Counter counter = Manager.counters[i];
 
+                    //For the lenght of the counters queue
                     for (int j = 0; j < Manager.counters[i].CounterLugageQueue.Length; j++)
                     {
+                        //lock the thread on the counters queue/buffer.
                         if (Monitor.TryEnter(counter.CounterLugageQueue))
                         {
+                            //Selects a random flightplan
                             int randomFlightplanIndex = random.Next(0, Manager.flightPlans.Length);
+
+                            //If the flightplan on the random flightplan isnt null
                             if (Manager.flightPlans[randomFlightplanIndex] != null)
                             {
+                                //lock on the flightplan
                                 if (Monitor.TryEnter(Manager.flightPlans[randomFlightplanIndex]))
                                 {
                                     string lugageNumber = Manager.flightPlans[randomFlightplanIndex].PlaneNumber.ToString() + random.Next(0, 50).ToString();
@@ -44,8 +52,8 @@ namespace Lugagesorting
                                         Monitor.Wait(counter.CounterLugageQueue, 2000);
                                     }
 
-                                    //Monitor.PulseAll(Manager.flightPlans[randomFlightplanIndex]);
-                                    //Monitor.Exit(Manager.flightPlans[randomFlightplanIndex]);
+                                    Monitor.PulseAll(Manager.flightPlans[randomFlightplanIndex]);
+                                    Monitor.Exit(Manager.flightPlans[randomFlightplanIndex]);
                                 }
                                 Thread.Sleep(500);
                             }
